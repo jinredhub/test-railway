5267a844897ac7f0067cc165c88ce63ebac5ce64
Jin Redmond <jinredmond@gmail.com>
2026-04-23 16:37:27 -0700

    added server

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// public folder
+app.use(express.static("client/build"));
+
+app.get("*", function(req, res) {
+  // this is a way to send a file. It will create correct path for Mac & PC
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', "index.html"));
+});
+
+// routes
+// require("./routes/twitter-api-routes.js");
+
+app.listen(port, "0.0.0.0", function() {
+  console.log("listening on port", port);
+});
